bc315528579da64eeeb5cd9bd362e58da6edc5a1
Jacob Krall <jacob@jkrall.net>
2025-12-14 12:58:47 -0500

    day 1 part 2

--- src/day1.py
+++ src/day1.py
@@ -6,20 +6,31 @@ def main():
 
     dial = 50
 
-    # Part 1
     is_zero = 0
+    clicks_zero = 0
     with open(filename, "r") as f:
         for line in f:
-            print("LINE:", line)
             num = int(line[1:])
-            if line[0] == "R":
-                dial += num
-            else:
-                dial -= num
-            dial = dial % 100
+            while num != 0:
+                match line[0]:
+                    case 'L':
+                        dial -= 1
+                        if dial == 0:
+                            clicks_zero += 1
+                        if dial == -1:
+                            dial = 99
+                    case 'R':
+                        dial += 1
+                        if dial == 100:
+                            clicks_zero += 1
+                            dial = 0
+                num -= 1
             if dial == 0:
                 is_zero += 1
+    # Part 1
     print(is_zero)
+    # Part 2
+    print(clicks_zero)
 
 
 if __name__ == "__main__":
