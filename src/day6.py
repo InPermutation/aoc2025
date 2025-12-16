bddc716eeda5c66ee49c8b9d8bc782a6e1831e82
Jacob Krall <jacob@jkrall.net>
2025-12-16 14:17:52 -0500

    day 6 part 2

--- src/day6.py
+++ src/day6.py
@@ -5,7 +5,8 @@ def main():
 
     rows = []
     with open(filename, "r") as f:
-        rows = [row.split() for row in f]
+        lines = [line.strip("\r\n") for line in f]
+        rows = [row.split() for row in lines]
     width = len(rows[0])
 
     # start with multiplicative/additive identities:
@@ -19,6 +20,42 @@ def main():
                 answers[col] += int(row[col])
 
     print("Part 1", sum(answers))
+
+    # Part 2:
+    width = len(lines[0])
+    answers = [1 if v == '*' else 0 for v in rows[-1]]
+    ixop = len(rows[0]) - 1
+    nums = []
+
+    for x in range(width - 1, -1, -1):
+        num = 0
+        isempty = True
+        for y in range(len(lines) - 1):
+            ch = lines[y][x]
+            if ch != ' ':
+                isempty = False
+                num = num * 10 + int(ch)
+        if isempty:
+            if rows[-1][ixop] == '+':
+                for n in nums:
+                    answers[ixop] += n
+            else:
+                for n in nums:
+                    answers[ixop] *= n
+            nums = []
+            ixop -= 1
+        else:
+            nums.append(num)
+            num = 0
+    if any(nums):
+        if rows[-1][ixop] == '+':
+            for n in nums:
+                answers[ixop] += n
+        else:
+            for n in nums:
+                answers[ixop] *= n
+
+    print("Part 2", sum(answers))
 
 
 if __name__ == "__main__":
